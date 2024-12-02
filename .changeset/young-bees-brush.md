229553a2ebc94de7313d1b5a4fd7fc107737cc2a
Heb <xsh4k3@gmail.com>
2024-12-03 03:04:15 +0700

    feat: match only one condition (#10)

---
+++ .changeset/young-bees-brush.md
@@ -0,0 +1,6 @@
+---
+"@hebilicious/vue-switch-match": minor
+"@hebilicious/nuxt-switch-match": minor
+---
+
+Changed the behaviour of the switch component: when there's multiple matches, only the first one will be rendered.
--- packages/vue-switch-match/src/components/Match.ts
+++ packages/vue-switch-match/src/components/Match.ts
@@ -1,13 +1,9 @@
 import { type PropType, defineComponent } from "vue"
 
+export type WhenProp = boolean | (() => boolean)
 export const Match = defineComponent({
 	name: "Match",
-	props: {
-		when: {
-			type: [Boolean, Function] as PropType<boolean | (() => boolean)>,
-			required: true
-		}
-	},
+	props: { when: { type: Object as PropType<WhenProp>, required: true } },
 	setup(_, { slots }) {
 		return () => slots.default?.() ?? null
 	}
